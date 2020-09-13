9251916f08d80e2769a9d11c83dc8e3f76b4ddf6
erielrj <eriel.rj@gmail.com>
2020-09-13 11:55:22 -0300

    produto-form bem encaminhado

--- angularGameShop/src/app/model/produto.ts
+++ angularGameShop/src/app/model/produto.ts
@@ -1,9 +1,10 @@
 export class Produto {
-    nome: string;
-    categoria: string;
-    preco: number;
-    quantidade: number;
-    desc: string;
-    desenvolvedor: string;
-    ativo: boolean;
+    nome:string;
+    categoria:string;
+    preco:number;
+    quantidade:number;
+    desc:string;
+    desenvolvedor:string;
+    foto:string;
+    ativo:boolean;
 }
--- angularGameShop/src/app/view/produto-form/produto-form.component.ts
+++ angularGameShop/src/app/view/produto-form/produto-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Produto } from 'src/app/model/produto';
 
 @Component({
   selector: 'app-produto-form',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./produto-form.component.css']
 })
 export class ProdutoFormComponent implements OnInit {
+
+  public produto:Produto = new Produto;
 
   constructor() { }
 
