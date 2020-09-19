dc5a9e6e91e9a33b6a70f2b08835f853d9d272cb
erielrj <eriel.rj@gmail.com>
2020-09-19 16:25:24 -0300

    Validacao do usuario e parte dos enderecos, além de troca nos membros da classe produto

---
+++ angularGameShop/src/app/model/endereco.ts
@@ -0,0 +1,8 @@
+export class Endereco {
+    bairro: string;
+    cep: string;
+    complemento: string;
+    localidade: string;
+    logradouro: string;
+    uf: string;
+}
---
+++ angularGameShop/src/app/view/endereco-form/endereco-form.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Endereco } from 'src/app/model/endereco';
+import { UsuarioService } from 'src/app/services/usuario.service';
+
+@Component({
+  selector: 'app-endereco-form',
+  templateUrl: './endereco-form.component.html',
+  styleUrls: ['./endereco-form.component.css']
+})
+export class EnderecoFormComponent implements OnInit {
+public endereco:Endereco = new Endereco;
+public cep:string = "";
+
+  constructor(private UsuarioService:UsuarioService) { }
+
+  ngOnInit(): void {
+  }
+
+  buscaCEP(){
+    this.UsuarioService.getEndereco(this.cep).subscribe( // validar o retorno
+      res => {
+        this.endereco = res;
+        console.log(res)
+      },
+      err => console.log(err)
+    )
+  }
+}
